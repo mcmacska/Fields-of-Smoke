85143f04ce37981e0a5745a3a78956119ac41790
Roro <m4rton.rol4nd@gmail.com>
2026-07-06 18:32:39 +0200

    Updated BloodScreen and FriendMarker

--- Fields_of_Smoke/scripts/ui/blood_screen.gd
+++ Fields_of_Smoke/scripts/ui/blood_screen.gd
@@ -1,18 +1,26 @@
-extends TextureRect
+extends ColorRect
 
 @export var fade_speed := 1.0
 
 var current_alpha := 0.0
 
 func _ready() -> void:
-	modulate.a = 0.0
-
+	_set_shader_intensity(0.0)
 
 func _process(delta: float) -> void:
 	current_alpha = move_toward(current_alpha, 0.0, fade_speed * delta)
-	modulate.a = current_alpha
-
+	_set_shader_intensity(current_alpha)
 
 func show_damage(strength := 0.5):
-	current_alpha = clamp(strength, 0.0, 1.0)
-	modulate.a = current_alpha
+	current_alpha = clamp(strength + randf_range(-0.1, 0.1), 0.0, 1.0)
+	_set_shader_intensity(current_alpha)
+	
+	var rand_x = randf_range(-0.5, 0.5)
+	var rand_y = randf_range(-0.5, 0.5)
+	
+	if material and material is ShaderMaterial:
+		material.set_shader_parameter("random_offset", Vector2(rand_x, rand_y))
+
+func _set_shader_intensity(value: float) -> void:
+	if material and material is ShaderMaterial:
+		material.set_shader_parameter("blood_intensity", value)
