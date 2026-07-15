779e48b2f6cda0e60d770e4137f1d2e92a8d2cb0
MCCAT <42695936+mcmacska@users.noreply.github.com>
2026-07-15 20:14:05 +0200

    frag grenade damage falloff added
    new mg added

--- Fields_of_Smoke/scripts/weapons/frag_grenade_throwable.gd
+++ Fields_of_Smoke/scripts/weapons/frag_grenade_throwable.gd
@@ -1,15 +1,18 @@
 extends RigidBody3D
 
-@onready var explosion_effect = $Explosion
+@onready var explosion_effect: Node3D = $Explosion
+@onready var damage_collision: CollisionShape3D = $DamageArea/DamageCollision
 
-@export var damage: int = 50
+@export var base_damage: int = 150
 @export var fuse_time: float = 4.0
 @export var fuse_timer: Timer
+@export var radius: float = 10.0
 
 var bodies: Array[Node] = []
 
 
 func _ready() -> void:
+	damage_collision.shape.radius = radius
 	fuse_timer.wait_time = fuse_time
 	fuse_timer.start()
 
@@ -24,10 +27,15 @@ func _on_fuse_timeout() -> void:
 	explosion_effect.explode()
 	# apply damage
 	for b in bodies:
-		# TODO: damage falloff
-		b.health.take_damage(damage)
+		apply_damage(b)
 	queue_free()
 	
+
+func apply_damage(body: Node):
+	var distance = global_position.distance_to(body.global_position)
+	var t = inverse_lerp(radius, 0.0, distance)
+	var damage = base_damage * t
+	body.health.take_damage(max(damage, 0.0))
 
 
 func _on_damage_area_body_entered(body: Node3D) -> void:
--- Fields_of_Smoke/scripts/weapons/machine_gun.gd
+++ Fields_of_Smoke/scripts/weapons/machine_gun.gd
@@ -6,7 +6,7 @@ func _init():
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	accuracy = 0.01
-	damage = 16
+	damage = 18
 	fire_rate = 0.025
 	reload_speed = 4.0
 	current_ammo = 100
@@ -17,8 +17,10 @@ func _ready() -> void:
 	recoil_speed = 16.0
 	return_speed = 8.0
 	# position
-	hip_position = Vector3(0, 0.498, -0.12)
-	ads_position = Vector3(0, 0.498, -0.12)
+	hip_position = Vector3.ZERO
+	ads_position = Vector3.ZERO
+	#hip_position = Vector3(0.01, 0.315, 0.44)
+	#ads_position = Vector3(0.01, 0.315, 0.44)
 
 
 func trigger_held(camera_transform: Transform3D):
